4e9f1a2ccfb5610c4e6813bba12ead365d5e4ae2
DereK Han <derekhan206@gmail.com>
2017-05-09 13:15:35 -0700

    Adds extra credit swipe right for answer

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -49,6 +49,33 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
         AnswerText.isHidden = true
         AnswerImage.isHidden = true
         SubmitButton.isEnabled = false
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                if (SubmitButton.isEnabled) {
+                    SubmitAnswer(self)
+                }
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
